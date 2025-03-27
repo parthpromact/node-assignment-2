525a564dd4ca63a0c5acda61867d158959af95ae
parthpromact <parthpatel@promactinfo.com>
2025-03-27 14:28:07 +0530

    add comments

--- utils/Socket.js
+++ utils/Socket.js
@@ -1,8 +1,9 @@
 import { Server } from "socket.io";
 
-let onlineUsers = {};
+let onlineUsers = {}; // Initially Empty Object
 
 const socketHandler = (server) => {
+  // Cors Configuration
   const io = new Server(server, {
     cors: {
       origin: "*",
@@ -12,17 +13,21 @@ const socketHandler = (server) => {
     },
   });
 
+  // Trigger When Socket Connect
   io.on("connection", (socket) => {
+    // Add User Event Listen
     socket.on("add-user", (userId) => {
       onlineUsers[userId] = socket.id;
+      // Emit Online User after Adding User
       io.emit("online-user", Object.keys(onlineUsers));
     });
 
+    // Send Message Event Listen
     socket.on("send-msg", (msg) => {
-      console.log("Online User", onlineUsers);
       const { senderId, receiverId, content } = msg;
       const receiverSocketId = onlineUsers[receiverId];
       if (receiverSocketId) {
+        // If Receiver is Online Emit Message to Receiver Socket ID
         io.to(receiverSocketId).emit("receive-msg", {
           senderId,
           receiverId,
@@ -31,13 +36,16 @@ const socketHandler = (server) => {
       }
     });
 
+    // Event Listen When Socket Disconnect
     socket.on("disconnect", () => {
       for (const userId in onlineUsers) {
+        // Socket Id matched with disconnected User Remove it from Online Users
         if (onlineUsers[userId] == socket.id) {
           delete onlineUsers[userId];
           break;
         }
       }
+      // Emit Online User after Disconnect
       io.emit("online-user", Object.keys(onlineUsers));
     });
   });
